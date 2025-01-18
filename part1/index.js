3adbb68b6592c96e6122b08e684189e28273ea22
hyungjin kim <hjux07@gmail.com>
2025-01-18 09:51:56 +0900

    섹션2.1. 타입스크립트는 변수, 매개변수, 리턴값에 타입 붙이는 것

--- part1/index.js
+++ part1/index.js
@@ -1,2 +1,24 @@
 "use strict";
-let a = 1;
+const a = '5';
+const b = 5;
+const c = false;
+const d = undefined;
+const e = null;
+let f = 123;
+f = '123';
+f = true;
+// 고정값
+const g = true;
+const h = 4;
+function add(x, y) {
+    return x + y;
+}
+const add2 = (x, y) => x + y;
+// const obj: { lat: number, lon: number } = { lat: 37.5, lon: 127.5 };
+const obj = { lat: 37.5, lon: 127.5 };
+const arr1 = ['123', '456'];
+const arr2 = ['123', '456'];
+const arr3 = [1, 2, 3];
+const arr4 = [1, 2, 3];
+// 튜플
+const arr5 = [123, 456, 'hi'];
--- part1/index.ts
+++ part1/index.ts
@@ -1,1 +1,45 @@
-let a: number = 1;
+const a: string = '5';
+const b: number = 5;
+const c: boolean = false;
+const d: undefined = undefined;
+const e: null = null;
+
+let f: any = 123;
+    f = '123';
+    f = true;
+
+
+// 고정값
+const g: true = true;
+const h: 4 = 4;
+
+
+function add(x: number, y: number): number {
+  return x + y 
+}
+
+// const add2: (x: number, y: number) => number = (x, y) => x + y;
+
+// type Add = (x: number, y: number) => number;
+interface AddInterface{
+  (x: number, y: number): number
+}
+
+const add2: AddInterface = (x, y) => x + y;
+
+interface objInterface {
+  lat: number,
+  lon: number,
+}
+// const obj: { lat: number, lon: number } = { lat: 37.5, lon: 127.5 };
+const obj: objInterface = { lat: 37.5, lon: 127.5 };
+
+const arr1: string[] = ['123', '456'];
+const arr2: Array<string> = ['123', '456'];
+
+const arr3: number[] = [1, 2, 3];
+const arr4: Array<number> = [1, 2, 3];
+
+// 튜플
+const arr5: [number, number, string] = [ 123, 456, 'hi'];
+
