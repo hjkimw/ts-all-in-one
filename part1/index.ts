9a9ff5d3cce31d9dae6160c8203bb9a86cf7dbad
hyungjin kim <hjux07@gmail.com>
2025-01-18 10:14:13 +0900

    섹션2.2. 타입 추론을 적극 활용하기

--- part1/index.ts
+++ part1/index.ts
@@ -1,8 +1,16 @@
-const a: string = '5';
-const b: number = 5;
-const c: boolean = false;
-const d: undefined = undefined;
-const e: null = null;
+// const a: string = '5';
+// const b: number = 5;
+// const c: boolean = false;
+// const d: undefined = undefined;
+// const e: null = null;
+
+// * 타입 추론
+const a = '5';
+const b = 5;
+const c = false;
+const d = undefined;
+const e = null;
+
 
 let f: any = 123;
     f = '123';
@@ -17,6 +25,7 @@ const h: 4 = 4;
 function add(x: number, y: number): number {
   return x + y 
 }
+const result = add(1, 2); // : number
 
 // const add2: (x: number, y: number) => number = (x, y) => x + y;
 
@@ -28,18 +37,27 @@ interface AddInterface{
 const add2: AddInterface = (x, y) => x + y;
 
 interface objInterface {
-  lat: number,
-  lon: number,
+  lat: number;
+  lon: number;
 }
 // const obj: { lat: number, lon: number } = { lat: 37.5, lon: 127.5 };
 const obj: objInterface = { lat: 37.5, lon: 127.5 };
 
-const arr1: string[] = ['123', '456'];
-const arr2: Array<string> = ['123', '456'];
 
-const arr3: number[] = [1, 2, 3];
-const arr4: Array<number> = [1, 2, 3];
+// const arr1: string[] = ['123', '456'];
+// const arr2: Array<string> = ['123', '456'];
+
+// const arr3: number[] = [1, 2, 3];
+// const arr4: Array<number> = [1, 2, 3];
 
 // 튜플
-const arr5: [number, number, string] = [ 123, 456, 'hi'];
+// const arr5: [number, number, string] = [ 123, 456, 'hi'];
 
+// * 타입 추론
+const arr2 = ['123', '456']; // string[]
+const arr4 = [1, 2, 3]; // number[]
+
+// 튜플
+const arr5 = [ 123, 456, 'hi']; // (string | number)[]
+
+const obj2 = { lat: 37.5, lon: 127.5 }; // { lat: number; lon: number; }
