97fddf3851a50b50afa4a94277df2d9c5d75cc3d
hyungjin kim <hjux07@gmail.com>
2025-02-06 14:07:48 +0900

    섹션 3.1. forEach, map 메서드 타입 제네릭 분석

--- part2/index.js
+++ part2/index.js
@@ -1,1 +1,26 @@
 "use strict";
+class C {
+}
+// * forEach 제네릭 분석
+{
+    /**
+     * 콜백의 매개변수 value에 value: T 형태로 제네릭을 설정해 순회하는 요소의 타입에 따라서 타입이 선언되고 있다.
+     * -> callbackFn: (value: T, index: number, array: T[])=> void
+     */
+    const arr1 = [1, 2, 3];
+    arr1.forEach(value => { console.log(value); });
+    // -> value: number
+    const arr2 = ['1', '2', '3'];
+    arr2.forEach(value => { console.log(value); });
+    // -> value: string 
+    const arr3 = ['1', 1, true];
+    ['1', 1, true].forEach(value => { console.log(value); });
+    // -> value: string | number | boolean
+    function add(x, y) {
+        return x;
+    }
+    // type parameter로 제네릭에 타입을 전달.
+    add(1, 2);
+    add('1', '2');
+    add(true, false);
+}
---
+++ part2/index.ts
@@ -0,0 +1,66 @@
+
+// * 제네릭
+interface A<T>{
+  // ...
+}
+
+type B<T> = {
+  // ...
+};
+
+class C<T>{
+  // ...    
+}
+
+
+// * forEach 메서드 타입 제네릭 분석
+{
+  interface Array<T>{
+    forEach(callbackFn: (value: T, index: number, array: T[])=> void, thisArg?: any): void;    
+  }
+  
+  /**
+   * 콜백의 매개변수 value에 value: T 형태로 제네릭을 설정해 순회하는 요소의 타입에 따라서 타입이 선언되고 있다.
+   * -> callbackFn: (value: T, index: number, array: T[])=> void
+   */
+
+  const arr1: Array<number> = [1, 2, 3]; 
+  arr1.forEach( value =>{ console.log(value)}); 
+  // -> value: number
+  
+  const arr2: Array<string> = ['1', '2', '3']; 
+  arr2.forEach( value =>{ console.log(value)});
+  // -> value: string 
+  
+  const arr3: Array<string | number | boolean> = ['1', 1, true]; 
+  ['1', 1, true].forEach( value =>{ console.log(value)}); 
+  // -> value: string | number | boolean
+
+
+  function add<T>(x: T, y: T): T { 
+    return x; 
+  }
+
+  // type parameter로 제네릭에 타입을 전달.
+  add<number>(1,2);
+  add<string>('1', '2');
+  add<boolean>(true,false);
+}
+
+
+// * map 메서드 타입 제네릭 분석
+{
+
+  interface Array<T>{
+    //...
+    map<U>(callback: (value: T, index: number, array: T[])=> U, thisArg?: any): U [];
+    //...
+  }
+
+  // 1. interface Array<T>의 제네릭 <T>가 number타입이 된다. 
+  //  1.1. <T> -> number
+  // 2. interface Array<T>의 map<U> 속성 제네릭 <U>가 string 타입이 된다. 
+  //  2.2. <U> -> string
+  const strings = [1, 2, 3].map(item => item.toString()); // string[]
+  
+}
