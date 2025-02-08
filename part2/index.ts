6af1e0bf1718ac44695a274a088637851234061e
hyungjin kim <hjux07@gmail.com>
2025-02-08 19:07:45 +0900

    섹션 3.4. filter 타입 직접 만들기

--- part2/index.ts
+++ part2/index.ts
@@ -121,4 +121,23 @@ class C<T>{
   const f = e.map(v=> +v); // number[]
 
   
+
+}
+
+
+// * filter 타입 직접 만들기
+{
+
+  interface Arr<T>{
+    filter<S extends T>(callbackFn: (value: T, index: number)=>  value is S): S[];
+  }
+
+  const a: Arr<number> = [1,2,3];
+
+  const b = a.filter((v): v is number => v % 2 === 0); // [2], number[]
+
+  const c: Arr<number | string> = [1, '2', 3, '4'];
+  const d = c.filter( (v): v is string => typeof v === 'string'); // ['2', '4'], string[]
+  const e = c.filter((v): v is number => typeof v === 'number'); // [1, 3, 5], number[]  
+
 }
