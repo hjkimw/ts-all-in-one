10d114735cd0273f17d8c7b74373f95bc4f1defd
hyungjin kim <hjux07@gmail.com>
2025-02-10 19:00:39 +0900

    섹션 4.2. Pick 타입 분석

--- part3/index.ts
+++ part3/index.ts
@@ -49,3 +49,28 @@
   };
 
 }
+
+
+// * Pick 타입 분석
+// -> 객체의 여러 속성중에서 특정 속성 타입만 추출
+{
+
+  interface Profile{
+    name: string;
+    age: number;
+    married: false;
+  }
+  
+  // 1. 제네릭 T, S 두 개를 전달받고, S는 전달받은 T의 key로 제한한다.
+  //  1.1. 제네릭 T는 속성을 추출할 대상 객체 타입이고, S는 추출할 속성 이름을 Union(|) 타입으로 전달받는다.
+  // 2. S의 각 key에 대해 T에서 해당 key의 값을 가져와 새로운 객체를 생성한다.
+  type newPick<T, S extends keyof T> = {
+    [key in S]: T[key];
+  };
+
+  const newJin: newPick<Profile, 'name' | 'age'> = {
+    name: 'jin',
+    age: 24,
+  };
+
+}
